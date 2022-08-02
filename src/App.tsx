bce904ebb67cc95dded1c5dc8fb55e5a74fc79c2
Vítor Ribeiro <85264247+itsflashii@users.noreply.github.com>
2022-08-01 22:08:34 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import logo from './assets/logo.svg'
+
+export const App = () => (
+    <>
+        <img src={logo} alt=""  />
+        <div className="container">
+            <p>“Aqui sua imaginação se torna realidade. Projetos, sonhos são o que movem a grande engrenagem da Varsel, venha crescer e se tornar a melhor em sua área.”</p>
+            <h5>Novidades em breve.</h5>
+        </div>
+    </>
+)
